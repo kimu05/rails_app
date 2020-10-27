240a737ad983a3a1f3b3a087997b60ee99544a6f
木村昌平 <kimu@kimu.local>
2020-10-28 08:38:45 +0900

    add unicorn

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,8 +1,8 @@
 # set lets
   $worker  = 2
   $timeout = 30
-  $app_dir = "/var/www/rails/sample_app" 
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $app_dir = "/Users/kimu/app/rails_app"
+  $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
   # set config
